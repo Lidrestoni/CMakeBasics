b4724c438f063822b3ba9473990c5d5d65702d3b
Guilherme Antunes da Silva <guilherme.antunes@uffs.edu.br>
2017-02-12 02:12:56 -0200

    Fourth Submit
    We'll try something new in here. Things aren't working out the best way, so we'll have to try our best.

--- ann/ann.cpp
+++ ann/ann.cpp
@@ -1,15 +1,62 @@
 #include <stdio.h>
+#include <vector>
 #include <opencv2/opencv.hpp>
 #include "cv.h"
 #include "ml.h"
 
-using namespace cv;
-using namespace ml;
+
+cv::Ptr<cv::ml::ANN_MLP> getTrainedNeuralNetwork(const cv::Mat& trainSamples, const cv::Mat& trainResponses)
+{
+  int networkInputSize = trainSamples.cols;
+  int networkOutputSize = trainResponses.cols;
+  cv::Ptr<cv::ml::ANN_MLP> mlp = cv::ml::ANN_MLP::create();
+  std::vector<int> layerSizes = { networkInputSize, networkInputSize / 2, networkOutputSize };
+  mlp->setLayerSizes(layerSizes);
+  mlp->setActivationFunction(cv::ml::ANN_MLP::SIGMOID_SYM);
+  mlp->train(trainSamples, cv::ml::ROW_SAMPLE, trainResponses);
+  return mlp;
+}
 
 
 int main(int argc, char** argv )
 {
-class other : public cv::ml::ANN_MLP{};
+	cv::ml::ANN_MLP *mlp;
+	//cv::ANN_MLP_TrainParams *params;
+
+/*class Ann : public cv::ml::ANN_MLP{
+	void setTrainMethod(int method, double param1 = 0, double param2 = 0){}
+	int getTrainMethod() const{}
+	void setActivationFunction(int type, double param1 = 0, double param2 = 0){}
+	void setLayerSizes(cv::InputArray _layer_sizes){}
+	cv::Mat getLayerSizes() const{}
+	cv::TermCriteria getTermCriteria() const{}
+	void setTermCriteria(cv::TermCriteria val){}
+	double getBackpropWeightScale() const{}
+	void setBackpropWeightScale(double val){}
+	double getBackpropMomentumScale() const{}
+	void setBackpropMomentumScale(double val){}
+	double getRpropDW0() const{}
+	void setRpropDW0(double val){}
+	double getRpropDWPlus() const{}
+	void setRpropDWPlus(double val){}
+	double getRpropDWMinus() const{}
+	void setRpropDWMinus(double val){}
+	double getRpropDWMin() const{}
+	void setRpropDWMin(double val){}
+	double getRpropDWMax() const{}
+	void setRpropDWMax(double val){}
+	cv::Mat getWeights(int layerIdx) const{}
+
+	int getVarCount() const{}
+	bool isTrained() const{}
+	bool isClassifier() const{}
+	float predict( cv::InputArray samples, cv::OutputArray results=cv::noArray(), int flags=0 ) const{}
+};
+cv::Mat mt(600,11,CV_8UC1);
+Ann *mlp = cv::ml::ANN_MLP::create();//mt, 1, 0, 0);*/
+
+
+//Ann mlp(const Mat& layerSizes, int activateFunc=CvANN_MLP::SIGMOID_SYM, double fparam1=0, double fparam2=0 );
 /*CvANN_MLP mlp;
 CvANN_MLP::create();
 CvANN_MLP::CvANN_MLP();
@@ -23,24 +70,7 @@ CvANN_MLP_TrainParams::CvANN_MLP_TrainParams()
 }*/
 
 
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
-
-    Mat image;
-    image = imread( argv[1], 1 );
-
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-
-    waitKey(0);
+   
 
     return 0;
 }
