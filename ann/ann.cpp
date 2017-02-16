2892182cf7080512fb5c11d979b782f47cc94d8e
Lidrestoni <guilherme.antunes@uffs.edu.br>
2017-02-16 15:22:20 -0200

    Random num generator working!
    We know have a Random Num generator and we're able to get a decent prototype in time.

--- ann/ann.cpp
+++ ann/ann.cpp
@@ -1,46 +1,73 @@
+/*
+* Acadêmicos : Andrey Vinicius Fagundes e Guilherme Antunes da Silva
+*/
 #include <stdio.h>
 #include <vector>
 #include <iostream>
+/*Bibliotecas OpenCV: */
 #include <opencv2/opencv.hpp>
 #include "cv.h"
 #include "ml.h"
+/*Bibliotecas usadas na shuffleMatRows: */
+#include <ctime>  
+#include <cstdlib> 
 
-cv::Ptr<cv::ml::ANN_MLP> getTrainedNeuralNetwork(const cv::Mat_<float>& trainSamples, const cv::Mat_<float>& trainResponses)
-{
-  int networkInputSize = trainSamples.cols; 
-  int networkOutputSize = trainResponses.cols;
-  cv::Ptr<cv::ml::ANN_MLP> mlp = cv::ml::ANN_MLP::create();
-  std::vector<int> layerSizes = { networkInputSize, networkInputSize / 2, networkOutputSize };
-  mlp->setLayerSizes(layerSizes);
-  mlp->setActivationFunction(cv::ml::ANN_MLP::SIGMOID_SYM);
- mlp->train(trainSamples, cv::ml::ROW_SAMPLE, trainResponses);
-  return mlp;
+cv::Ptr<cv::ml::ANN_MLP> getTrainedNeuralNetwork(const cv::Mat_<float>& trainSamples, const cv::Mat_<float>& trainResponses){
+	int networkInputSize = trainSamples.cols; 
+	int networkOutputSize = trainResponses.cols;
+	cv::Ptr<cv::ml::ANN_MLP> mlp = cv::ml::ANN_MLP::create();
+	std::vector<int> layerSizes = { networkInputSize, networkInputSize / 2, networkOutputSize };
+	mlp->setLayerSizes(layerSizes);
+	mlp->setActivationFunction(cv::ml::ANN_MLP::SIGMOID_SYM);
+	mlp->train(trainSamples, cv::ml::ROW_SAMPLE, trainResponses);
+	return mlp;
 }
 
 
-int main(int argc, char** argv )
-{
-	cv::Mat_<float> trainSamples(1, 10, CV_32FC1);
-	cv::Mat_<float> trainResponses(1, 1, CV_32FC1);
-	trainSamples.reserve(4000);
-	trainResponses.reserve(4000);
+void FILEtoMAT(char *name, cv::Mat_<float>& input, cv::Mat_<float>& output){
+	input = cv::Mat_<float>(1, 10, CV_32FC1);
+	output = cv::Mat_<float>(1, 1, CV_32FC1);	
+	FILE *file = fopen(name, "r");	
+	input.reserve(4000);
+	output.reserve(4000);
 	float in[11], out;
 	cv::Mat_<float> mat;
-	while(scanf("%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n", &in[0], &in[1], &in[2], &in[3], &in[4], &in[5], &in[6], &in[7], &in[8], &in[9], &out)!=EOF){
+	while(fscanf(file,"%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n", &in[0], &in[1], &in[2], &in[3], &in[4], &in[5], &in[6], &in[7], &in[8], &in[9], &out)!=EOF){
 			mat = cv::Mat_<float>(1, 10, CV_32FC1);
 			mat.reserve(2);
 			for(int i=0; i<10;i++)
 				mat.at<float>(0,i) = in[i];
-			trainSamples.push_back(mat);
+			input.push_back(mat);
 			mat = cv::Mat_<float>(1, 1, CV_32FC1);
 			mat.reserve(2);
 			mat.at<float>(0,0) = out;
-			trainResponses.push_back(mat);
+			output.push_back(mat);
 	}
-	cv::Ptr<cv::ml::ANN_MLP> mlp = getTrainedNeuralNetwork(trainSamples, trainResponses);
-	cv::Mat_<float> temp(1, 1, CV_32FC1);
-	 mlp->predict(trainSamples, temp);
+	fclose(file);
+}
 
-	std::cout << temp;
-    return 0;
+void shuffleMatRows(const cv::Mat &origInput, const cv::Mat &origOutput, cv::Mat &shuffInput, cv::Mat &shuffOutput){
+/*Essa função cria um vetor, no qual são colocados números de bilhetes de 0 a n-1 (onde n é o número de linha de uma das matrizes de origem, pois ambas devem ter o mesmo número de linhas).
+* Então esses números são embaralhados, e as matrizes de saída são montadas seguindo essa ordem de bilhetes sorteados
+*/
+	std::vector <int> tickets;
+	for (int i = 0; i < origInput.rows; i++)
+		tickets.push_back(i);
+	std::srand ( unsigned ( std::time(0) ) );
+	std::random_shuffle ( tickets.begin(), tickets.end() );
+	for (int i = 0; i < origInput.rows; i++){
+		shuffInput.push_back(origInput.row(tickets[i]));
+		shuffOutput.push_back(origOutput.row(tickets[i]));
+	}
+}
+
+int main(int argc, char** argv ){	
+	cv::Mat_<float> originalInput, originalOutput, trainingInput, trainingOutput;
+	FILEtoMAT((char *)"data", originalInput, originalOutput);
+	shuffleMatRows(originalInput, originalOutput, trainingInput, trainingOutput);
+	cv::Ptr<cv::ml::ANN_MLP> mlp = getTrainedNeuralNetwork(originalInput, originalOutput);
+	cv::Mat_<float> temp(1, 1, CV_32FC1);
+	mlp->predict(originalInput, temp);
+
+	return 0;
 }
