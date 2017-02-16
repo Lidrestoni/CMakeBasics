8b42c2f668b35d1c3097fd6081a1657ae164b9d2
Lidrestoni <guilherme.antunes@uffs.edu.br>
2017-02-16 11:30:22 -0200

    Training done?
    It seems everything is working just nice. Let's see whether it's really gonna work

--- ann/ann.cpp
+++ ann/ann.cpp
@@ -47,41 +47,38 @@
 
 
 
-cv::Ptr<cv::ml::ANN_MLP> getTrainedNeuralNetwork(const cv::Mat_<double>& trainSamples, const cv::Mat_<double>& trainResponses)
+cv::Ptr<cv::ml::ANN_MLP> getTrainedNeuralNetwork(const cv::Mat_<float>& trainSamples, const cv::Mat_<float>& trainResponses)
 {
-  int networkInputSize = trainSamples.cols;
+  int networkInputSize = trainSamples.cols; 
   int networkOutputSize = trainResponses.cols;
   cv::Ptr<cv::ml::ANN_MLP> mlp = cv::ml::ANN_MLP::create();
   std::vector<int> layerSizes = { networkInputSize, networkInputSize / 2, networkOutputSize };
   mlp->setLayerSizes(layerSizes);
   mlp->setActivationFunction(cv::ml::ANN_MLP::SIGMOID_SYM);
-  //mlp->train(trainSamples, cv::ml::ROW_SAMPLE, trainResponses);
-mlp->train(trainSamples, cv::ml::ROW_SAMPLE, trainSamples);
+ mlp->train(trainSamples, cv::ml::ROW_SAMPLE, trainResponses);
   return mlp;
 }
 
 
 int main(int argc, char** argv )
 {
-	cv::Mat_<double> trainSamples(1, 10, CV_32FC1);
-	cv::Mat_<double> trainResponses(1, 1, CV_32FC1);
+	cv::Mat_<float> trainSamples(1, 10, CV_32FC1);
+	cv::Mat_<float> trainResponses(1, 1, CV_32FC1);
 	trainSamples.reserve(4000);
 	trainResponses.reserve(4000);
-	double in[11], out;
-	cv::Mat_<double> mat;
-	while(scanf("%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf\n", &in[0], &in[1], &in[2], &in[3], &in[4], &in[5], &in[6], &in[7], &in[8], &in[9], &out)!=EOF){ //printf("%lf\n", in[0]);
-			mat = cv::Mat_<double>(1, 10, CV_32FC1);
+	float in[11], out;
+	cv::Mat_<float> mat;
+	while(scanf("%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n", &in[0], &in[1], &in[2], &in[3], &in[4], &in[5], &in[6], &in[7], &in[8], &in[9], &out)!=EOF){
+			mat = cv::Mat_<float>(1, 10, CV_32FC1);
 			mat.reserve(2);
 			for(int i=0; i<10;i++)
-				mat.at<double>(0,i) = in[i];
+				mat.at<float>(0,i) = in[i];
 			trainSamples.push_back(mat);
-			mat = cv::Mat_<double>(1, 1, CV_32FC1);
+			mat = cv::Mat_<float>(1, 1, CV_32FC1);
 			mat.reserve(2);
-			mat.at<double>(0,0) = out;
+			mat.at<float>(0,0) = out;
 			trainResponses.push_back(mat);
-			//printf("%lf\n", trainSamples.at<double>(cv::Point(0,0)));
 	}
-	std::cout <<trainSamples;
 	cv::Ptr<cv::ml::ANN_MLP> mlp = getTrainedNeuralNetwork(trainSamples, trainResponses);
 
 	//cv::ANN_MLP_TrainParams *params;
